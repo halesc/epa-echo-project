fa1730acbfc09ba6d723031000b39f408312502f
andrewzinkan <andrew.zinkan@braincorp.com>
2023-10-17 16:56:15 -0700

    interactive mapping

--- src/folium_test.py
+++ src/folium_test.py
@@ -33,49 +33,7 @@ air_pollution_geojson = air_pollution_shapefile.to_json()
 air_pollution_layer = folium.GeoJson(air_pollution_geojson)
 air_pollution_layer.add_to(m)
 
-# Define a custom JavaScript function to show/hide London data point
-filter_js = """
-<script>
-    var map = L.map("map");
-    var airPollutionLayer = %s;
 
-    airPollutionLayer.addTo(map);
-
-    var showLondon = true;
-
-    var kingCountyBounds = L.latLngBounds(L.latLng(47.3, -122.5), L.latLng(47.8, -121.9));
-
-    map.fitBounds(kingCountyBounds);
-
-    L.control.layers(null, { "Air Pollution": airPollutionLayer }).addTo(map);
-
-    var londonMarker;
-
-    function filterData(feature) {
-        return kingCountyBounds.contains(L.latLng(feature.geometry.coordinates[1], feature.geometry.coordinates[0]));
-    }
-
-    function toggleLondonMarker() {
-        if (showLondon) {
-            londonMarker = L.marker([51.5074, -0.1278], {
-                icon: L.divIcon({
-                    className: 'london-marker',
-                    iconSize: [30, 30],
-                    html: '<i class="fa fa-map-marker fa-2x"></i>',
-                }),
-            });
-            londonMarker.addTo(map);
-        } else {
-            map.removeLayer(londonMarker);
-        }
-        showLondon = !showLondon;
-    }
-
-    L.easyButton('fa-eye', toggleLondonMarker, 'Show/Hide London Data').addTo(map);
-</script>
-""" % air_pollution_geojson
-
-m.get_root().html.add_child(folium.Element(filter_js))
 
 m.save("../maps/interactive_map_with_show_hide_button.html")
 
