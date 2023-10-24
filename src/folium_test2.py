69234b6754e6425bbf52cbd5082501c22f6d13d4
andrewzinkan <andrew.zinkan@braincorp.com>
2023-10-23 18:59:25 -0700

    Adding all points wiht filters an mapping

--- src/folium_test2.py
+++ src/folium_test2.py
@@ -1,26 +1,24 @@
 import folium
 from folium.plugins import TagFilterButton
-import pandas as pd
-from folium.plugins import TagFilterButton
 from folium.plugins import Search
 from folium.plugins import LocateControl
+import pandas as pd
 import numpy as np
-import random
-import branca
 import geopandas
 
-def create_filtered_map(df, lat_col, lon_col, filter_cols):
+def create_filtered_map(df, lat_col, lon_col, filter_cols, geospatial_layers=None, layer_names=None):
     """
-    Create a Folium map with filtered data points and interactive filters.
+    Create a Folium map with filtered data points, interactive filters, and geospatial layers.
 
     Parameters:
         df (pandas.DataFrame): The DataFrame containing data to be plotted.
         lat_col (str): The name of the column containing latitude coordinates.
         lon_col (str): The name of the column containing longitude coordinates.
         filter_cols (list of str): List of column names for which you want to create filters.
+        geospatial_layers (list of geopandas.GeoDataFrame, optional): List of geospatial layers to add.
 
     Returns:
-        folium.Map: The Folium map with data points and interactive filters.
+        folium.Map: The Folium map with data points, interactive filters, and geospatial layers.
     """
     # Create a base map
     m = folium.Map(location=[df[lat_col].mean(), df[lon_col].mean()], zoom_start=5)
@@ -34,9 +32,17 @@ def create_filtered_map(df, lat_col, lon_col, filter_cols):
         latlng = (row[lat_col], row[lon_col])
         popup = ", ".join([f"{col}: {row[col]}" for col in df.columns])
 
+        # Create a custom HTML icon with a red exclamation mark
+        icon_html = """
+        <div style="font-size: 8px; color: red;">
+          <i class="fas fa-exclamation-circle"></i>
+        </div>
+        """
+        
         folium.Marker(
             location=latlng,
-            popup=popup
+            popup=popup,
+            icon=folium.DivIcon(html=icon_html)
         ).add_to(data_layer)
 
     # Create filters for specified columns
@@ -47,13 +53,17 @@ def create_filtered_map(df, lat_col, lon_col, filter_cols):
         # Create a TagFilterButton for each filter
         TagFilterButton(categories_sorted, name=col).add_to(m)
 
-    # Add a layer control to toggle the visibility of the data points and filters
+    # Add geospatial layers if provided
+    if geospatial_layers and layer_names:
+        for i, layer in enumerate(geospatial_layers):
+            folium.GeoJson(layer, name= layer_names[i]).add_to(m)
+
+    # Add a layer control to toggle the visibility of the data points, filters, and geospatial layers
     folium.LayerControl(collapsed=False).add_to(m)
 
     return m
 
-
-######################## Citation Data ####################################
+######################## DATA PREP ####################################
 df = pd.read_csv('../lib/processed/tidy_data.csv')
 df.head()
 
@@ -69,18 +79,22 @@ df = df.dropna(subset=['lat', 'long'])
 df.columns
 
 # Specify the columns for which you want to create filters
-filter_columns = ['primary_law', 'state', 'county', 'air_pollutant_class_code', 'democrat', 'independent', 'libertarian',
+filter_columns = ['primary_law', 'state', 'democrat', 'independent', 'libertarian',
        'republican']
 
-# Create the Folium map with filters
-filtered_map = create_filtered_map(df, 'lat', 'long', filter_columns)
+# Load the geospatial layers
+states = geopandas.read_file(
+    "https://raw.githubusercontent.com/PublicaMundi/MappingAPI/master/data/geojson/us-states.json",
+    driver="GeoJSON",
+)
 
+cities = geopandas.read_file(
+    "https://d2ad6b4ur7yvpq.cloudfront.net/naturalearth-3.3.0/ne_50m_populated_places_simple.geojson",
+    driver="GeoJSON",
+)
 
-
-
-
+# Create the Folium map with filters and geospatial layers
+filtered_map = create_filtered_map(df, 'lat', 'long', filter_columns, geospatial_layers=[states, cities], layer_names = ["states", "cities"])
 
 # Save or display the map
 filtered_map.save('filtered_map.html')
-
-
