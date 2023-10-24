d11c015f690f81c47373ae6bf2e21caf3c410072
andrewzinkan <andrew.zinkan@braincorp.com>
2023-10-23 22:24:53 -0700

    working city and state search + heat map

--- src/folium_test.py
+++ src/folium_test.py
@@ -1,3 +1,4 @@
+# https://gispub.epa.gov/air/trendsreport/2021/#pm2_5_composition
 # Medium: https://medium.com/p/e3aff3b0ed43
 
 # Word Press: https://waterprogramming.wordpress.com/2023/04/05/creating-interactive-geospatial-maps-in-python-with-folium/
@@ -21,6 +22,16 @@
 # https://python-visualization.github.io/folium/latest/advanced_guide.html
 
 
+# https://medium.com/planet-os/analyzing-air-quality-data-from-planet-os-datahub-using-python-pandas-and-plotly-f2766c003c6c
+
+
+# Options:
+# https://openaq.org/
+
+# DataSources
+# Full list: https://github.com/openaq/awesome-air-quality
+# AWS Example: https://github.com/openaq/openaq-api-v2
+# BigQuery: https://medium.com/@Faraz_EA/analyzing-epa-air-quality-data-using-bigquery-and-choropleth-map-f1c59b0406f4
 
 import pandas as pd
 import folium
@@ -177,7 +188,7 @@ statesearch = Search(
 
 citysearch = Search(
     layer=citygeo,
-    geom_type="Polygon",
+    geom_type="Point",
     placeholder="Search for a US City",
     collapsed=False,
     search_label="nameascii",
